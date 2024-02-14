2b79f753b22c20bcc1e44434e7fe2ec4cd434459
Vijay Prakash Mishra <vijaymishra.07dec@gmail.com>
2024-02-14 15:11:24 +0530

    first commit

---
+++ basics.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+    int a = 2;
+    char b = 'k';
+    double c = 25;
+    float d = 3.14;
+    short e = 20;
+    long f = 123456789;
+
+    scanf("%c", &a);
+
+    printf("Hello World");
+    printf("%c", a);
+
+    return 0;
+}
